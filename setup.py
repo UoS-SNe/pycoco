fa2e5933b8ffb1e42db1f4d90b703b4a656e1a04
Rob Firth <ref1g12@soton.ac.uk>
2017-03-24 15:43:30 +0000

    rearrange

--- setup.py
+++ setup.py
@@ -18,6 +18,22 @@ versionRegExp = re.compile("__version__ = \"(.*?)\"")
 
 # Assign to __version__
 __version__ =  versionRegExp.findall(s)[0]
+
+## Compile list of data files to be moved to .local
+data_files = [("lc", [ os.path.join(packageDir, "data/lc", i) for i in os.listdir(os.path.join(packageDir, "data/lc"))] ),
+              ("filters", [ os.path.join(packageDir, "data/filters", i) for i in os.listdir(os.path.join(packageDir, "data/filters"))] )
+             ]
+
+for i in os.listdir(os.path.join(packageDir, "data/spec")):
+    path = os.path.join("spec/", i)
+    datalist = []
+    for j in os.listdir(os.path.join(packageDir, "data/", path)):
+        datalist.append(os.path.join(packageDir, "data", path, j))
+
+    datatuple = (path, datalist)
+    data_files.append(datatuple)
+
+
 setup(# package information
       name=packageName,
       version=__version__,
@@ -30,7 +46,7 @@ setup(# package information
                 packageName+".sims",
                 packageName+".utils",
                 ],
-      package_dir={packageName:'pycoco'}
+      package_dir={packageName:'pycoco'},
 
-      # What data to include as packages
+      data_files = data_files
       )
