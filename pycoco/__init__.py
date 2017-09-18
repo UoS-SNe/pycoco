569db13a88b003ba737a29d93a924d1e6d328557
Rob Firth <r.firth@soton.ac.uk>
2017-09-18 13:18:10 +0100

    tidied up __init__.py

--- pycoco/__init__.py
+++ pycoco/__init__.py
@@ -22,27 +22,6 @@ except NameError:
 
     __file__ = sys.argv[0]
 
-# import os
-# import warnings
-# import unittest
-# import subprocess
-# # import httplib ## use http.client on python3 - not using at the mo
-# # from urlparse import urlparse
-# import re
-# from collections import OrderedDict
-#
-# import astropy as ap
-# import astropy.units as u
-# from astropy.constants import c
-# from astropy.time import Time
-# from astropy.table import Table, vstack
-#
-# import numpy as np
-# from matplotlib import pyplot as plt
-# from matplotlib.ticker import MultipleLocator
-# from scipy.interpolate import InterpolatedUnivariateSpline
-# from scipy.interpolate import interp1d as interp1d
-
 from .extinction import *
 from .colours import *
 from .utils import *
