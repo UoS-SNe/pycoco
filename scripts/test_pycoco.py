cbe5f928ae30d93465b6ea34a53fbbc4dd3537c9
Rob Firth <ref1g12@soton.ac.uk>
2017-11-23 14:08:15 +0000

    adding test for specphot - see Issue #29

--- scripts/test_pycoco.py
+++ scripts/test_pycoco.py
@@ -15,7 +15,7 @@ import unittest
 import pycoco as pcc
 import astropy.units as u
 import matplotlib as mpl
-
+import numpy as np
 #  #------------------------------------#  #
 #  #  TESTING                           #  #
 #  #------------------------------------#  #
@@ -181,11 +181,30 @@ class TestClass(unittest.TestCase):
         x = pcc.classes.SpectrumClass()
         self.assertEqual(os.path.abspath(os.path.join(x._get_data_directory(), os.pardir)), os.path.abspath(pcc.defaults._default_data_dir_path))
 
-    def test_SpectrumClass_get_data_dir_returns_default_spec_dir(self):
-        x = pcc.classes.SpectrumClass()
-        self.assertEqual(os.path.abspath(x._get_data_directory()), os.path.abspath(os.path.join(pcc.defaults._default_data_dir_path, "spec/")))
+    # def test_SpectrumClass_get_data_dir_returns_default_spec_dir(self):
+    #     x = pcc.classes.SpectrumClass()
+    #     self.assertEqual(os.path.abspath(x._get_data_directory()), os.path.abspath(os.path.join(pcc.defaults._default_data_dir_path, "spec/")))
 
-    # specfitClass
+    def test_SpectrumClass_get_specphot_works_more_than_one_overlapping_filter(self):
+
+    def test_SpectrumClass_get_specphot_works_with_one_overlapping_filter(self):
+
+                snname = "SN1993J"
+
+        listfile = os.path.join(pcc.defaults._default_list_dir_path, snname+".list")
+
+        sn = pcc.classes.SNClass(snname)
+        sn.load_phot(path=os.path.join(pcc.defaults._default_data_dir_path, "lc/" + snname + ".dat"))
+        sn.load_list(listfile)
+        sn.load_spec()
+        sn.check_overlaps()
+        S = sn.spec["1993J_-11.0.txt"]
+        S.get_specphot([sn.phot.data_filters["BessellV"], ], verbose=True)
+        self.assertAlmostEqual(1.37e-17, S.specphot["flux"][np.where(S.specphot["filter"] == "BessellV")[0][0]])
+
+
+
+        # specfitClass
 
     # def test_specfitClass
 
